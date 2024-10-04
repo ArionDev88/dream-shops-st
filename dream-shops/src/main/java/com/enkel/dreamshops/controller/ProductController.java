d30f0b6c6f33d5fea7f56d5b388d35ee7f10121f
Enkel007 <eshehdula22@epoka.edu.al>
2024-10-04 20:36:52 +0200

    added RoleRepository, implemented user authentication

--- dream-shops/src/main/java/com/enkel/dreamshops/controller/ProductController.java
+++ dream-shops/src/main/java/com/enkel/dreamshops/controller/ProductController.java
@@ -10,6 +10,7 @@ import com.enkel.dreamshops.response.ApiResponse;
 import com.enkel.dreamshops.service.product.IProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -41,6 +42,7 @@ public class ProductController {
         }
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping("/add")
     public ResponseEntity<ApiResponse> addProduct(@RequestBody AddProductRequest product){
         try {
@@ -53,6 +55,7 @@ public class ProductController {
         }
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PutMapping("/product/{productId}/update")
     public ResponseEntity<ApiResponse> updateProduct(@RequestBody ProductUpdateRequest request, @PathVariable Long productId){
         try {
@@ -65,6 +68,7 @@ public class ProductController {
         }
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @DeleteMapping("/product/{productId}/delete")
     public ResponseEntity<ApiResponse> deleteProduct(@PathVariable Long productId){
         try {
--- dream-shops/src/main/java/com/enkel/dreamshops/data/DataInitializer.java
+++ dream-shops/src/main/java/com/enkel/dreamshops/data/DataInitializer.java
@@ -1,23 +1,38 @@
 package com.enkel.dreamshops.data;
 
 import com.enkel.dreamshops.model.User;
+import com.enkel.dreamshops.model.Role;
+import com.enkel.dreamshops.repository.RoleRepository;
 import com.enkel.dreamshops.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import java.util.Set;
+
+@Transactional
 @Component
 @RequiredArgsConstructor
 public class DataInitializer implements ApplicationListener<ApplicationReadyEvent> {
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public void onApplicationEvent(ApplicationReadyEvent event) {
+        Set<String> defaultRoles = Set.of("ROLE_ADMIN", "ROLE_USER");
         createDefaultUserIfNotExists();
+        createDefaultRoleIfNotExists(defaultRoles);
+        createDefaultAdminIfNotExists();
     }
 
     private void createDefaultUserIfNotExists() {
+        Role userRole = roleRepository.findByName("ROLE_USER").get();
+
         for(int i = 1; i <= 5; i++){
             String defaultEmail = "user" + i + "@gmail.com";
             if(userRepository.existsByEmail(defaultEmail)){
@@ -27,10 +42,37 @@ public class DataInitializer implements ApplicationListener<ApplicationReadyEven
             user.setFirstName("User");
             user.setLastName("McUser" + i);
             user.setEmail(defaultEmail);
-            user.setPassword("123456");
+            user.setPassword(passwordEncoder.encode("123456"));
+            user.setRoles(Set.of(userRole));
             userRepository.save(user);
 
             System.out.println("Default user " + i + " created successfully.");
         }
+    }
+
+    private void createDefaultAdminIfNotExists() {
+        Role adminRole = roleRepository.findByName("ROLE_ADMIN").get();
+
+        for(int i = 1; i <= 2; i++){
+            String defaultEmail = "admin" + i + "@gmail.com";
+            if(userRepository.existsByEmail(defaultEmail)){
+                continue;
+            }
+            User user = new User();
+            user.setFirstName("Admin");
+            user.setLastName("McAdmin" + i);
+            user.setEmail(defaultEmail);
+            user.setPassword(passwordEncoder.encode("123456"));
+            userRepository.save(user);
+            user.setRoles(Set.of(adminRole));
+
+            System.out.println("Default admin " + i + " created successfully.");
+        }
+    }
+
+    private void createDefaultRoleIfNotExists(Set<String> roles) {
+        roles.stream()
+                .filter(role -> roleRepository.findByName(role).isEmpty())
+                .map(Role::new).forEach(roleRepository::save);
     }
 }
